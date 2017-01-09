36ec865384ae4e2dfc3ccc0a66251e70179faaca
Edwin <edwin.j.acevedo.01@gmail.com>
2017-01-09 15:06:14 +0000

    javascript day one
    q

--- script.js
+++ script.js
@@ -1,1 +1,77 @@
 //Write your function calls below this line
+setColor(5, 5, "blue");
+setColor(4, 5, "blue");
+setColor(5, 4, "blue");
+setColor(4, 4, "blue");
+setColor(0, 0, "blue");
+setColor(9, 0, "blue");
+setColor(0, 9, "blue");
+setColor(9, 9, "blue");
+
+//a smiley face made of coordinates
+
+// setColor(0, 2, "blue");
+// setColor(1, 2, "blue");
+// setColor(2, 2, "blue");
+// setColor(0, 7, "blue");
+// setColor(1, 7, "blue");
+// setColor(2, 7, "blue");
+// setColor(5, 0, "blue");
+// setColor(6, 1, "blue");
+// setColor(7, 2, "blue");
+// setColor(7, 3, "blue");
+// setColor(7, 4, "blue");
+// setColor(7, 5, "blue");
+// setColor(7, 6, "blue");
+// setColor(7, 7, "blue");
+// setColor(6, 8, "blue");
+// setColor(5, 9, "blue");
+
+//the letter "s" made of coordinates
+
+// setColor(0, 3, "blue");
+// setColor(0, 4, "blue");
+// setColor(0, 5, "blue");
+// setColor(1, 6, "blue")
+// setColor(1, 2, "blue"); 
+// setColor(2, 2, "blue");
+// setColor(3, 2, "blue");
+// setColor(4, 3, "blue");
+// setColor(4, 4, "blue");
+// setColor(4, 5, "blue");
+// setColor(5, 6, "blue");
+// setColor(6, 6, "blue");
+// setColor(7, 6, "blue");
+// setColor(8, 5, "blue");
+// setColor(8, 4, "blue");
+// setColor(8, 3, "blue");
+// setColor(7, 2, "blue")
+
+//colombian flag made of coordinates 
+
+// setColor(3, 5, "yellow");
+// setColor(4, 5, "yellow");
+// setColor(5, 5, "blue");
+// setColor(6, 5, "red");
+// setColor(3, 6, "yellow");
+// setColor(4, 6, "yellow");
+// setColor(5, 6, "blue");
+// setColor(6, 6, "red");
+// setColor(3, 4, "yellow");
+// setColor(4, 4, "yellow");
+// setColor(5, 4, "blue");
+// setColor(6, 4, "red");
+// setColor(3, 3, "yellow");
+// setColor(4, 3, "yellow");
+// setColor(5, 3, "blue");
+// setColor(6, 3, "red");
+// setColor(3, 2, "yellow");
+// setColor(4, 2, "yellow");
+// setColor(5, 2, "blue");
+// setColor(6, 2, "red");
+// setColor(3, 7, "yellow");
+// setColor(4, 7, "yellow");
+// setColor(5, 7, "blue");
+// setColor(6, 7, "red");
+
+
